b08e37c63ed9ae30a92b21dba53c56c37a9388e9
vmalepati1 <malepativikas@gmail.com>
2024-01-05 15:15:38 -0500

    Started centroid algorithm to find beat drops

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ from moviepy.editor import VideoFileClip
 import re
 import librosa
 import matplotlib.pyplot as plt
+import sklearn.preprocessing
+
+def normalize(x, axis = 0):
+  return sklearn.preprocessing.minmax_scale(x, axis = axis)
 
 audio_tiktok_link = 'https://www.tiktok.com/@glintsfx/video/7296966811646430506?q=edits&t=1704346855251'
 ##
@@ -38,11 +42,40 @@ print(f'Sampling Rate: {sampling_rate} Hz')
 # Collapse stereo audio array into a mono audio array with 1 channel
 mono_audio = np.mean(audio_array, axis=1)
 
+print(f'Mono audio shape: {mono_audio.shape}')
+
 X = librosa.stft(mono_audio)
 X_db = librosa.amplitude_to_db(abs(X))
 
-# Draw spectogram
-plt.figure(figsize = (10, 5))
+# Plot spectogram
+plt.figure(figsize=(10, 5))
 librosa.display.specshow(X_db, sr = sampling_rate, x_axis = 'time', y_axis = 'hz')
 plt.colorbar(format='%+2.0f dB')
+
+# Compute spectral centoids (average frequency at each time)
+spectral_centroids = librosa.feature.spectral_centroid(y=mono_audio, sr=sampling_rate)[0]
+print(f'Spectral centroids audio shape: {spectral_centroids.shape}')
+
+plt.figure(figsize=(12, 4))
+frames = range(len(spectral_centroids))
+
+t = librosa.frames_to_time(frames, sr=sampling_rate)
+
+# Plot spectral centroids
+librosa.display.waveshow(mono_audio, sr = sampling_rate, alpha = 0.4)
+plt.plot(t, normalize(spectral_centroids), color = 'b')
+plt.title('Waveform with Spectral Centroids')
+plt.xlabel('Time (s)')
+plt.ylabel('Amplitude / Spectral Centroids')
 plt.show()
+
+# Percentile within which frequencies this low are considered beat drops
+beat_drop_centroid_thresh = 5
+
+threshold = np.percentile(spectral_centroids, beat_drop_centroid_thresh)
+
+beat_drop_indices = np.where(spectral_centroids < threshold)[0]
+
+beat_drop_timestamps = librosa.frames_to_time(beat_drop_indices, sr=sampling_rate)
+
+print(beat_drop_timestamps)
